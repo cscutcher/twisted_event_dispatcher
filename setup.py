1ceaf567cc11c0010693977b935a63091f9964a2
Chris Scutcher <cscutche@cisco.com>
2015-03-25 17:16:18 +0000

    Updated setup metadata

--- setup.py
+++ setup.py
@@ -7,10 +7,11 @@ from setuptools import setup, find_packages
 setup(
     name='twisted_event_dispatcher',
     author='Chris Scutcher',
-    author_email='chris.scutcher@ninebysix.co.uk',
+    author_email='cscutche@cisco.com',
     url='https://github.com/cscutcher/twisted_event_dispatcher',
     version='0.1',
     packages=find_packages(),
     install_requires=('twisted', 'zope.interface', 'mock'),
     namespace_packages = ['oni'],
+    description='Experimental generic event dispatcher written for twisted.'
 )
